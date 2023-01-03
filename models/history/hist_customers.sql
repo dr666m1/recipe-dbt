819b9dd654bd1ea5d5530d1500d118d8e86ac147
dr666m1 <skndr666m1@gmail.com>
2023-01-03 17:11:07 +0900

    fmt

--- models/history/hist_customers.sql
+++ models/history/hist_customers.sql
@@ -5,23 +5,26 @@
 }}
 
 with
-  customers as (
-    select * from {{ ref("stg_dummy__customers") }}
-    where snapshot_dt = snapshot_dt_
-  ),
-  user_orders as (
-    select
-      customer_id,
-      sum(amount) as ltv,
-      min(order_dt) as first_order_dt,
-    from {{ ref("stg_dummy__orders")}}
-    where snapshot_dt = snapshot_dt_
-    group by 1
-  )
+customers as (
+  select *,
+  from {{ ref("stg_dummy__customers") }}
+  where snapshot_dt = snapshot_dt_
+),
+
+user_orders as (
+  select
+    customer_id,
+    sum(amount) as ltv,
+    min(order_dt) as first_order_dt,
+  from {{ ref("stg_dummy__orders") }}
+  where snapshot_dt = snapshot_dt_
+  group by 1
+)
+
 select
   c.*,
-  coalesce(uo.ltv, 0) as ltv,
   uo.first_order_dt,
+  coalesce(uo.ltv, 0) as ltv,
 from
   customers as c
-  left join user_orders as uo using(customer_id)
+left join user_orders as uo on c.customer_id = uo.customer_id
--- models/staging/stg_dummy__customers.sql
+++ models/staging/stg_dummy__customers.sql
@@ -1,27 +1,49 @@
 with
-  snapshot_20230102 as (
-    select
-      id as customer_id,
-      * except (id),
-    from
-      unnest(
-        [
-          struct("a" as id, date "2023-01-01" as register_dt, "JP" as country),
-          struct("b" as id, date "2023-01-02" as register_dt, "US" as country)
-        ]
-      ) as data
-  ),
-  snapshot_20230103 as (
-    select * from snapshot_20230102
-    union all
-    select
-      id as customer_id,
-      * except (id),
-    from
-      unnest(
-        [struct("c" as id, date "2023-01-03" as register_dt, "JP" as country)]
-      ) as data
-  )
-select *, date "2023-01-02" as snapshot_dt from snapshot_20230102
+snapshot_20230102 as (
+  select
+    id as customer_id,
+    register_dt,
+    country,
+  from
+    unnest(
+      [
+        struct(
+          "a" as id, date "2023-01-01" as register_dt, "JP" as country
+        ),
+        struct(
+          "b" as id, date "2023-01-02" as register_dt, "US" as country
+        )
+      ]
+    )
+),
+
+snapshot_20230103 as (
+  select
+    customer_id,
+    register_dt,
+    country,
+  from snapshot_20230102
+  union all
+  select
+    id as customer_id,
+    register_dt,
+    country,
+  from
+    unnest(
+      [
+        struct(
+          "c" as id, date "2023-01-03" as register_dt, "JP" as country
+        )
+      ]
+    )
+)
+
+select
+  *,
+  date "2023-01-02" as snapshot_dt,
+from snapshot_20230102
 union all
-select *, date "2023-01-03" as snapshot_dt from snapshot_20230103
+select
+  *,
+  date "2023-01-03" as snapshot_dt,
+from snapshot_20230103
