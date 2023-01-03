eecdddb8f48184a8dc8d6c5f76feadf98f96b299
dr666m1 <skndr666m1@gmail.com>
2023-01-03 18:45:29 +0900

    add comments

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+# check if this script is run by github actions
 if [[ -v CI ]]; then
+  # do not access BigQuery
   sqlfluff lint models \
     --dialect bigquery \
     --templater jinja \
